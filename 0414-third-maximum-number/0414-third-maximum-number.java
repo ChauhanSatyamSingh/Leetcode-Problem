a98226f0356ad4e2ffcdf98d9e48e0b356308fa3
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-03-31 06:27:29 +0530

    Time: 3 ms (77.63%), Space: 44.9 MB (56.88%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,36 +1,26 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Integer max = null,
+        Integer max1 = null,
                 max2 = null,
                 max3 = null;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (max != null && max == nums[i])
+        for (int x : nums) {
+            if ((max1 != null && max1 == x) || (max2 != null && max2 == x) || (max3 != null && max3 == x))
                 continue;
 
-            if (max2 != null && max2 == nums[i])
-                continue;
-
-            if (max3 != null && max3 == nums[i])
-                continue;
-
-            if (max == null || nums[i] > max) {
+            if(max1 == null || x > max1) {
                 max3 = max2;
-                max2 = max;
-                max = nums[i];
-            }
-            else if (max2 == null || nums[i] > max2) {
+                max2 = max1;
+                max1 = x;
+            }else if(max2 == null || x > max2) {
                 max3 = max2;
-                max2 = nums[i];
-            }
-            else if (max3 == null || nums[i] > max3) {
-                max3 = nums[i];
+                max2 = x;
+            }else if(max3 == null || x > max3) {
+                max3 = x;
             }
         }
 
-        if (max3 != null)
-            return max3;
-
-        return max;
+        if(max3 != null) return max3;
+        return max1;
     }
 }
