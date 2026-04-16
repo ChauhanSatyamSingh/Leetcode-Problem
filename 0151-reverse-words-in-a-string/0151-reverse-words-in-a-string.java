0b31cc35afbdb1f3130d3ec1ad9b881782fdc6d3
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-04-16 14:30:50 +0530

    Time: 8 ms (38.7%), Space: 44 MB (88.16%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,19 +1,75 @@
 class Solution {
     public String reverseWords(String s) {
-        // approach 1
-        // "  hello world  "
-        String trimmedString = s.trim();
-        // "hello world"
+        // "  hello  world  "
+        //  l              r
+        int left = 0;
+        int right = s.length() - 1;
 
-        String[] arrStr = trimmedString.split("\\s+");
-        // ["hello", "world"]
+        while(left < s.length() && s.charAt(left) == ' ') left++;
+        // "  hello  world  "
+        //    l            r
 
-        List<String> list = Arrays.asList(arrStr);
-        // <"hello", "world">
+        while(right >= 0 && s.charAt(right) == ' ') right--; 
+        // "  hello  world  "
+        //    l          r
 
-        Collections.reverse(list);
-        // <"world", "hello">
+        StringBuilder sb = new StringBuilder();
+        while(left <= right) {
+            if(s.charAt(left) != ' ') sb.append(s.charAt(left));
+            else if(s.charAt(left - 1) != ' ') sb.append(s.charAt(left));
 
-        return String.join(" ", list);
+            left++;
+            
+        }
+        // sb=hello world
+
+        int i = 0;
+        int j = sb.length() - 1;
+
+        while(i < j) {
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, temp);
+
+            i++;
+            j--;
+        }
+        // sb=dlrow olleh
+        //    s
+        //    e
+
+        int st = 0;
+        int end = 0;
+
+        while(st < sb.length()) {
+            while(end < sb.length() && sb.charAt(end) != ' ') end++;
+                // sb=dlrow olleh
+                //    s
+                //         e
+
+            // Reverse between s and (e-1)
+            i = st;
+            j = end - 1;
+            while(i < j) {
+                char temp = sb.charAt(i);
+                sb.setCharAt(i, sb.charAt(j));
+                sb.setCharAt(j, temp);
+
+                i++;
+                j--;
+            }
+            // sb=world olleh
+            //      i
+            //      j
+
+            st = end + 1;
+            end = end + 1;
+            // sb=dlrow olleh
+            //          s
+            //          e
+
+        }
+
+        return sb.toString();
     }
 }
