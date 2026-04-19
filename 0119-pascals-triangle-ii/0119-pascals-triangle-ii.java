8db707c0737b5135bab60f7d7cb2bc8e544469dd
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-04-19 06:21:14 +0530

    Time: 0 ms (100%), Space: 42.5 MB (56.3%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,14 +1,11 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-
-        //use combination logic: (prev col val)*(n-r+1)/r
-        //n --> rowIndex, r --> coloum no(i)
-
         List<Integer> row = new ArrayList<>();
         row.add(1);
+
         for(int i = 1; i <= rowIndex; i++) {
-                int currEle = (int)(row.get(i - 1) * (long)(rowIndex - i + 1) / i);
-                row.add(currEle);
+            int curr = (int)(row.get(i-1) * (long)(rowIndex - i + 1) / i);
+            row.add(curr);
         }
 
         return row;
