ac85ea7db5cac970ee320f3b63772e02bd946fe8
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-04-19 06:05:51 +0530

    Time: 5 ms (99.87%), Space: 48.4 MB (97.95%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,27 +1,26 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        //count sort
-        int k = 10000;
 
+        int k = 10000;
         int[] count = new int[2 * k + 1];
 
         for(int x: nums) {
             count[x + k]++;
         }
 
-        //perform sorting
         int idx = 0;
         for(int i = 0; i < count.length; i++) {
             while(count[i] > 0) {
                 nums[idx++] = i - k;
+
                 count[i]--;
             }
         }
 
-        //finding maximized sum
         int sum = 0;
-        for(int i = 0; i < nums.length; i += 2)
+        for(int i = 0; i < nums.length; i += 2) {
             sum += nums[i];
+        }
 
         return sum;
     }
