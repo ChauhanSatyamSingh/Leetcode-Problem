9bf9cb9c294158c983b088b9b6c17f6e9de37dcb
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-04-22 05:42:14 +0530

    Time: 1 ms (100%), Space: 43.5 MB (89.28%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,19 +1,12 @@
 class Solution {
     public int myAtoi(String s) {
-        //"    1234" --> 1234
-        //"    -234" --> -234
-        //"    +234" --> 234
-        //"  -045" --> -45
-        //"  -we323" --> 0
         int i = 0;
-        int num = 0;
 
-        int sign = 1; //default is +ve, -1 for -ve
+        //Ignoring leading whitespace
+        while(i < s.length() && s.charAt(i) == ' ') i++;
 
-        // 1. Ignore leading white space
-        while(i < s.length() && s.charAt(i) == ' ') i++; 
-
-        // 2. Signedness
+        //Handling sign +/-
+        int sign = 1;
         if(i < s.length()) {
             if(s.charAt(i) == '-') {
                 sign = -1;
@@ -21,26 +14,26 @@ class Solution {
             } else if(s.charAt(i) == '+') i++;
         }
 
-        // 3. Conversion
+        //Conversion & Rounding
+        int num = 0;
+
         while(i < s.length() && Character.isDigit(s.charAt(i))) {
             int digit = s.charAt(i) - '0';
 
-            // 4. Rounding
-            // Im....0.....IM
-            // -2147483648
-            // 2147483647
-            if(num == Integer.MAX_VALUE/10) {
+            if(num == Integer.MAX_VALUE / 10) {
                 if(sign == 1) {
                     if(digit >= 7) return Integer.MAX_VALUE;
-                } else if(sign == -1) {
+                }
+                else {
                     if(digit >= 8) return Integer.MIN_VALUE;
                 }
             }
 
-            if(num > Integer.MAX_VALUE/10) {
+            if(num > Integer.MAX_VALUE / 10) {
                 if(sign == 1) return Integer.MAX_VALUE;
                 else return Integer.MIN_VALUE;
             }
+
             num = num * 10 + digit;
             i++;
         }
