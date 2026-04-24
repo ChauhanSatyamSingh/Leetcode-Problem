66574c42de4299f1b25d38a7dfd3f68cab5aaa0c
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-04-24 23:15:02 +0530

    Time: 1 ms (99.94%), Space: 52.8 MB (64.16%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,50 +11,41 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tA = headA;
-        ListNode tB = headB;
-
-        //count no. element in headA
-        int cA = 0;
-        while (tA != null) {
-            cA++;
-            tA = tA.next;
-        }
-
-        //counting no. element in headB
-        int cB = 0;
-        while (tB != null) {
-            cB++;
-            tB = tB.next;
-        }
-
+        //hogya
         ListNode a = headA;
         ListNode b = headB;
 
-        //taking list with more no. elements
-        if (cA < cB)
+        int cA = 1;
+        while(a != null) {
+            cA++;
+            a = a.next;
+        }
+
+        int cB = 1;
+        while(b != null) {
+            cB++;
+            b = b.next;
+        }
+
+        if(cB < cA) 
             return getIntersectionNode(headB, headA);
 
-        //calculation differecne in no. of elements
-        int diff = cA - cB;
+        int diff = cB - cA;
 
-        //move a until diff greater than 0
-        while (diff > 0) {
-            a = a.next;
+        a = headA;
+        b = headB;
+        while(diff >= 1) {
+            b = b.next;
             diff--;
         }
 
-        //now move both a and b till null
-        while (a != null && b != null) {
-            //if they match at any element return that
-            if (a == b)
-                return a;
-                
+        while(a != null && b != null) {
+            if(a == b) return a;
+
             a = a.next;
             b = b.next;
         }
 
-        //if not matched return null
         return null;
     }
 }
