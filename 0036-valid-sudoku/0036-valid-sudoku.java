40a23d35453708f3b933074fb3c2ea1128fda140
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-04-28 08:52:26 +0530

    Time: 1 ms (99.9%), Space: 46.2 MB (84.02%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,17 +4,13 @@ class Solution {
         int[][] cols = new int[9][9];
         int[][] boxes = new int[9][9];
 
-        for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board[0].length; c++) {
-
+        for(int r = 0; r < board.length; r++) {
+            for(int c = 0; c < board[0].length; c++) {
                 if(board[r][c] == '.') continue;
-
                 int val = board[r][c] - '1';
-                int boxIdx = (r / 3) * 3 + (c / 3);
+                int boxIdx = (r/3) * 3 + (c/3);
 
-                if (rows[r][val] == 1 || cols[c][val] == 1 || boxes[boxIdx][val] == 1)
-                    return false;
-
+                if(rows[r][val] == 1 || cols[c][val] == 1 || boxes[boxIdx][val] == 1) return false;
                 rows[r][val] = 1;
                 cols[c][val] = 1;
                 boxes[boxIdx][val] = 1;
