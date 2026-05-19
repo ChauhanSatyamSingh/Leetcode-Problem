ae694eae5045dda9f17a6aea9ef2f785b0d1c5bf
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-05-19 10:32:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (90.99%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,19 +1,11 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < stones.length(); i++) {
-            char c = stones.charAt(i);
-            if(!map.containsKey(c))
-                map.put(c, 1);
-            else map.put(c, map.get(c)+1);
-        }
-
         int res = 0;
+
         for(int i = 0; i < jewels.length(); i++) {
-            char c = jewels.charAt(i);
-            if(map.containsKey(c))
-                res += map.get(c);
+            for(int j = 0; j < stones.length(); j++) {
+                if(stones.charAt(j) == jewels.charAt(i)) res++;
+            }
         }
 
         return res;
