645b98791e5cdf50e2c89237d5b18b907bdb013b
Satyam <142412463+ChauhanSatyamSingh@users.noreply.github.com>
2026-06-04 22:18:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.24%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -2,18 +2,18 @@ class Solution {
     public int heightChecker(int[] heights) {
         int[] exp = new int[101];
 
-        for(int h: heights) {
+        for(int h: heights) 
             exp[h]++;
-        }
-
+        
         int count = 0;
         int j = 0;
         for(int i = 0; i < exp.length; i++) {
             while(exp[i] > 0) {
-                if(i != heights[j]) count++;
-                j++;
+                if(i != heights[j])
+                    count++;
 
                 exp[i]--;
+                j++;
             }
         }
 
